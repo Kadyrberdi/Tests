589dec5eb0cf57ffabdc2c5cbe6b0c86e91fe7fb
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-12-03 21:42:56 +0300

    feat: add validator to phone

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 'use strict';
+const form = document.querySelector('.order'),
+    phone = document.querySelectorAll('#phone'),
+    btn = document.querySelector('.order__btn');
+
+phone.forEach(elem => {
+    const validator = function () {
+        elem.value = elem.value.replace(/[^0-9+]/ig, '');
+    };
+    elem.addEventListener('input', validator);
+});
